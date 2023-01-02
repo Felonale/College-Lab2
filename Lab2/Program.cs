54fca0f039ea75b0cb7bd4676c6a74ddbfb2ae08
Felonale <danilko.04@gmail.com>
2023-01-02 21:28:53 +0600

    Final changes for 1.0

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -8,6 +8,17 @@ namespace Lab2
 {
     class Program
     {
+        public class Info
+        {
+            public static void About()
+            {
+                Console.WriteLine("About Lab\n" +
+                    "Copyright © 2022-2022 by\n" +
+                    "Felonale(Lapenko Danil)\n" +
+                    "Published by Felonale\n" +
+                    "https://github.com/Felonale/College-Lab2");
+            }
+        }
         public class Exercises //Класс для заданий
         {
             public static void First()
@@ -225,7 +236,7 @@ namespace Lab2
                 {
                     for (int j = 0; j < matrix.GetLength(1); j++)
                     {
-                        matrix[i, j] = rand.Next(-999, 999);
+                        matrix[i, j] = rand.Next(-99, 99);
                     }
                 }
             }
@@ -233,13 +244,11 @@ namespace Lab2
             public static void Fifth()
             {
                 Random random = new Random();
-                int[,] FirstMatrix = new int[random.Next(1, 10), random.Next(1, 10)];
+                int[,] FirstMatrix = new int[random.Next(2, 10), random.Next(2, 10)];
                 FullTwoDimMatrix(FirstMatrix);
-                int[,] SecondMatrix = new int[FirstMatrix.GetLength(1), random.Next(1, 10)]; //Произведение двух матриц выполнимо только в случае, если количество столбцов левой матрицы равно количеству строк правой.
+                int[,] SecondMatrix = new int[FirstMatrix.GetLength(1), random.Next(2, 10)]; //Произведение двух матриц выполнимо только в случае, если количество столбцов левой матрицы равно количеству строк правой.
                 FullTwoDimMatrix(SecondMatrix);
-                //Начало 1 условия
-                Console.WriteLine("1 условие: ");
-
+                
                 int sumDiagFirstMatrix = 0; //Сумма диагоналей первой матрицы
                 int dimMinFirstMatrix = FirstMatrix.GetLength(0) < FirstMatrix.GetLength(1) ? FirstMatrix.GetLength(0) : FirstMatrix.GetLength(1); //Минимальное измерение первого массива
                 for (int i = 0; i < dimMinFirstMatrix; i++)
@@ -254,7 +263,9 @@ namespace Lab2
                     sumDiagSecondMatrix += SecondMatrix[i, i];
                 }
 
-                if(sumDiagFirstMatrix > sumDiagSecondMatrix) //Если сумма элементов главной диагонали 1-ой матрицы больше суммы элементов главной диагонали 2 - ой матрицы
+                //Начало 1 условия
+                Console.WriteLine("1 условие: ");
+                if (sumDiagFirstMatrix > sumDiagSecondMatrix) //Если сумма элементов главной диагонали 1-ой матрицы больше суммы элементов главной диагонали 2 - ой матрицы
                                                              //то найти произведение этих матриц
                 {
                     int[,] ThirdMatrix = new int[FirstMatrix.GetLength(0),SecondMatrix.GetLength(1)]; //Матрица, в которой будут хранится данные после перемножения
@@ -302,24 +313,271 @@ namespace Lab2
                     Console.WriteLine("B:");
                 }
 
-                Console.WriteLine("Нажмите любую кнопку для продолжения: ");
+                Console.Write("Нажмите любую кнопку для продолжения");
+                Console.ReadKey();
                 //Конец 1 условия
+                //Начало 2 условия
+                Console.WriteLine("\n2 условие: ");
+                if (sumDiagFirstMatrix < sumDiagSecondMatrix)//если сумма элементов главной диагонали 1-ой матрицы меньше суммы элементов главной диагонали 2 - ой матрицы
+                                                            //то исходные матрицы необходимо транспонировать
+                {
+                    int[,] tempFirstMatrix = new int[FirstMatrix.GetLength(1), FirstMatrix.GetLength(0)]; //Создаём временные матрицы для транспонирования
+                    int[,] tempSecondMatrix = new int[SecondMatrix.GetLength(1), SecondMatrix.GetLength(0)];
+                    for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+                        {
+                            tempFirstMatrix[j,i] = FirstMatrix[i,j]; //Транспонируем матрицу, перенося результат во временную матрицу
+                        }
+                    }
+                    for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < SecondMatrix.GetLength(1); j++)
+                        {
+                            tempSecondMatrix[j, i] = SecondMatrix[i, j];
+                        }
+                    }
+
+                    FirstMatrix = new int[tempFirstMatrix.GetLength(0), tempFirstMatrix.GetLength(1)]; //Изменяем первоначальные матрицы на измерения транспонированных
+                    SecondMatrix = new int[tempSecondMatrix.GetLength(0), tempSecondMatrix.GetLength(1)];
+                    for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+                        {
+                            FirstMatrix[i, j] = tempFirstMatrix[i, j]; //Добавляем данные из ячеек временных массивов в первоначальные матрицы
+                        }
+                    }
+                    for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < SecondMatrix.GetLength(1); j++) 
+                        {
+                            SecondMatrix[i, j] = tempSecondMatrix[i, j];
+                        }
+                    }
+                    Console.WriteLine("Транспонированные матрицы:\n" +
+                        "A:");
+                    ShowMatrix(FirstMatrix);
+                    Console.WriteLine("B:");
+                    ShowMatrix(SecondMatrix);
+                }
+                else //иначе умножить исходные матрицы на число 5
+                {
+                    for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+                        {
+                            FirstMatrix[i, j] *= 5;
+                        }
+                    }
+                    for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < SecondMatrix.GetLength(1); j++)
+                        {
+                            SecondMatrix[i, j] *= 5;
+                        }
+                    }
+                    Console.WriteLine("Матрицы, умноженные на 5:\n" +
+                        "A:");
+                    ShowMatrix(FirstMatrix);
+                    Console.WriteLine("B:");
+                    ShowMatrix(SecondMatrix);
+                }
+
+                Console.Write("Нажмите любую кнопку для продолжения");
+                Console.ReadKey();
+                //Конец 2 условия
+                //Начало 3 условия
+                Console.WriteLine("\n3 условие: "); //в исходных матрицах найти максимальные и минимальные элементы и поменять их местами
+
+                // Ниже я попробовал сделать хранение данных о ячейке с максимальным числом через массив. Но в случае, когда у нас есть несколько ячеек, содержащие
+                // максимальное число, изменится только первое. В нашем случае это не подходит
+                /*int[] maxNum = new int[3] { 0, 0, 0 }; 
+                int[] minNum = new int[3] { 0, 0, 0 };*/
+
+                int maxNum = 0; //Создаём переменную, в которой будем хранить максимальное число
+                int minNum = 0; //Создаём переменную, в которой будем хранить максимальное число
+
+                //Меняем числа для 1 матрицы
+                for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+                    {
+                        maxNum = maxNum < FirstMatrix[i,j] ? FirstMatrix[i,j] : maxNum; //Если проверяемая сейчас ячейка больше, чем максимальное записанное число, вписываем новое значение
+                    }
+                }
+                for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+                    {
+                        minNum = minNum > FirstMatrix[i, j] ? FirstMatrix[i, j] : minNum; //Если проверяемая сейчас ячейка меньше, чем минимальное записанное число, вписываем новое значение
+                    }
+                }
+
+                for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+                    {
+                        if (FirstMatrix[i, j] == maxNum) //Если проверяемая ячейка - максимальное число, меняем его на минимальное
+                        {
+                            FirstMatrix[i, j] = minNum;
+                        }
+                        else if (FirstMatrix[i, j] == minNum)
+                        {
+                            FirstMatrix[i, j] = maxNum;
+                        }
+                    }
+                }
+                Console.WriteLine("Матрицы с изменёнными максимальными и минимальными числами:\n" +
+                        $"A (Max: {maxNum}, Min: {minNum}):");
+                ShowMatrix(FirstMatrix);
+
+                //Меняем числа для 2 матрицы
+                for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < SecondMatrix.GetLength(1); j++)
+                    {
+                        maxNum = maxNum < SecondMatrix[i, j] ? SecondMatrix[i, j] : maxNum; //Если проверяемая сейчас ячейка больше, чем максимальное записанное число, вписываем новое значение
+                    }
+                }
+                for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < SecondMatrix.GetLength(1); j++)
+                    {
+                        minNum = minNum > SecondMatrix[i, j] ? SecondMatrix[i, j] : minNum; //Если проверяемая сейчас ячейка меньше, чем минимальное записанное число, вписываем новое значение
+                    }
+                }
+
+                for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < SecondMatrix.GetLength(1); j++)
+                    {
+                        if (SecondMatrix[i, j] == maxNum) //Если проверяемая ячейка - максимальное число, меняем его на минимальное
+                        {
+                            SecondMatrix[i, j] = minNum;
+                        }
+                        else if (SecondMatrix[i, j] == minNum)
+                        {
+                            SecondMatrix[i, j] = maxNum;
+                        }
+                    }
+                }
+
+                Console.WriteLine($"B (Max: {maxNum}, Min: {minNum}:");
+                ShowMatrix(SecondMatrix);
+
+                Console.Write("Нажмите любую кнопку для продолжения");
+                Console.ReadKey();
+                //Конец 3 условия
+                //Начало 4 условия
+                Console.WriteLine("\n4 условие: "); //в исходных матрицах посчитать количество положительных и отрицательных элементов
+                int numOfPositive = 0;
+                int numOfNegative = 0;
+                for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+                    {
+                        if (FirstMatrix[i, j] > 0)
+                            numOfPositive++;
+                        else if (FirstMatrix[i, j] < 0)
+                            numOfNegative++;
+                    }
+                }
+                Console.WriteLine($"Количество положительных чисел в матрице A: {numOfPositive}");
+                Console.WriteLine($"Количество отрицательных чисел в матрице A: {numOfNegative}");
+                numOfPositive = 0;
+                numOfNegative = 0;
+                for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < SecondMatrix.GetLength(1); j++)
+                    {
+                        if (SecondMatrix[i, j] > 0)
+                            numOfPositive++;
+                        else if (SecondMatrix[i, j] < 0)
+                            numOfNegative++;
+                    }
+                }
+                Console.WriteLine($"Количество положительных чисел в матрице B: {numOfPositive}");
+                Console.WriteLine($"Количество отрицательных чисел в матрице B: {numOfNegative}");
+
+                Console.Write("Нажмите любую кнопку для продолжения");
+                Console.ReadKey();
+                //Конец 4 условия
+                //Начало 5 условия
+                Console.WriteLine("\n5 условие: "); //в исходных матрицах посчитать суммы элементов строк и столбцов
+                Console.WriteLine("Сумма строк и столбцов матриц:\n" +
+                    "A:");
+                int tempRow = 0;
+                int[] tempCollumn = new int[FirstMatrix.GetLength(1)]; //Создаём массив с размерностью второго измерения матрицы А
+                for (int i = 0; i < tempCollumn.Length; i++)
+                {
+                    tempCollumn[i] = 0; //Заполняем массив нулями
+                }
+
+                for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+                {
+                    Console.Write("\t"); //Добавляем табуляцию в начало каждой строки для того, чтобы строка "Sum", суммирующая столбцы, не съехала
+                    for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+                    {
+                        Console.Write(FirstMatrix[i, j] + "\t");
+                        tempRow += FirstMatrix[i, j]; //Добавляем во временную переменную числа, суммирующие строку
+                        tempCollumn[j] += FirstMatrix[i, j]; //Добавляем в каждую ячейку массива сумму от каждого стоблца (Так, как читка массива происходит
+                                                             //от по строкам, а не столбцам, мы заполяем дополнительную строку, которую будем выводить
+                                                             //в конце отображения массива)
+                    }
+                    Console.WriteLine("Sum: " + tempRow);
+                    tempRow = 0; //Сбрасываем временную переменную, суммирующую строку
+                }
+                Console.Write("Sum:\t");
+                for (int i = 0; i < tempCollumn.Length; i++)
+                {
+                    Console.Write(tempCollumn[i] + "\t");
+                }
+                Console.WriteLine("\n\nB:"); //Двойной перенос строки для форматирования
+
+                tempRow = 0;
+                tempCollumn = new int[SecondMatrix.GetLength(1)]; //Изменяем массив с размерностью второго измерения матрицы В
+                for (int i = 0; i < tempCollumn.Length; i++)
+                {
+                    tempCollumn[i] = 0; //Заполняем массив нулями
+                }
+
+                for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+                {
+                    Console.Write("\t"); //Добавляем табуляцию в начало каждой строки для того, чтобы строка "Sum", суммирующая столбцы, не съехала
+                    for (int j = 0; j < SecondMatrix.GetLength(1); j++)
+                    {
+                        Console.Write(SecondMatrix[i, j] + "\t");
+                        tempRow += SecondMatrix[i, j]; //Добавляем во временную переменную числа, суммирующие строку
+                        tempCollumn[j] += SecondMatrix[i, j];//Добавляем в каждую ячейку массива сумму от каждого стоблца (Так, как читка массива происходит
+                                                             //от по строкам, а не столбцам, мы заполяем дополнительную строку, которую будем выводить
+                                                             //в конце отображения массива)
+                    }
+                    Console.WriteLine("Sum: " + tempRow);
+                    tempRow = 0; //Сбрасываем временную переменную, суммирующую строку
+                }
+                Console.Write("Sum:\t");
+                for (int i = 0; i < tempCollumn.Length; i++)
+                {
+                    Console.Write(tempCollumn[i] + "\t");
+                }
+                //Конец 5 условия
             }
         }
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.Write("Введите номер задания 1-5(Введите exit для выхода): ");
+                Console.Write("Введите номер задания 1-5 (Введите 'exit' для выхода или 'about' для информации): ");
                 string variant = Console.ReadLine().Trim().ToLower();
                 switch (variant)
                 {
-                    case "1": Exercises.First(); break;
-                    case "2": Exercises.Second(); break;
-                    case "3": Exercises.Third(); break;
-                    case "4": Exercises.Fourth(); break;
-                    case "5": Exercises.Fifth(); break;
+                    case "1": Console.Clear(); Exercises.First(); break;
+                    case "2": Console.Clear(); Exercises.Second(); break;
+                    case "3": Console.Clear(); Exercises.Third(); break;
+                    case "4": Console.Clear(); Exercises.Fourth(); break;
+                    case "5": Console.Clear(); Exercises.Fifth(); break;
                     case "exit": Environment.Exit(0); break;
+                    case "about": Console.Clear(); Info.About(); break;
                     default: Console.WriteLine("Введен неверный номер задания"); break;
                 }
             }
